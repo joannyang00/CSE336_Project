8dbf1159b21f0ca599209a9f8f23dd8d9033049c
Joan Bae <joan.bae@stonybrook.edu>
2016-03-06 20:01:57 -0500

    first commit

---
+++ WebApplication3/src/java/com/javax/lex/SecurityQuesServlet.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javax.lex;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 배요한 (Joan Bae)
+ */
+public class SecurityQuesServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        String s = req.getParameter("seq");
+        if(s.equals("")){
+            out.println("Please choose one question");
+        }else{
+            out.print("ok");
+        }
+    }
+    
+    
+    
+}
---
+++ WebApplication3/src/java/com/javax/lex/countryServlet.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javax.lex;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 배요한 (Joan Bae)
+ */
+public class countryServlet extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        String s = req.getParameter("country");
+        if(s.equals("")){
+            out.println("Please choose one country");
+        }else{
+            out.print("ok");
+        }
+     
+     
+    }
+    
+    
+}
